4a8f96f4be7c1910a34cef69ecf856e0ff7e8bf0
Juan Andrés Diaz Higuita <juanandresdiazhiguita@MacBook-Pro-8.local>
2020-08-21 15:02:25 -0500

    Create crud for user controller

---
+++ src/main/java/wolox/training/exceptions/UserNotFoundException.java
@@ -0,0 +1,21 @@
+package wolox.training.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(){
+        super();
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+}
